893a16d05cb1f3364df417bf24382a85a8b37033
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-01 01:40:47 +0000

    variable declaration

--- basics/test.js
+++ basics/test.js
@@ -3,3 +3,24 @@ console.log(2+2)
 let name="Ayush"
 const Id=21
 console.log(name)
+const accountId=356
+let accountEmail="avnish@google.com"
+var accountPassword="12345"
+accountCity= "Jaipur"
+let accountState;//Undefined
+
+//accountId = 2 //not allowed as it is constant
+
+accountEmail = "avnish154@google.com"
+accountPassword = "23456"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+
+/*
+Prefer not to use var
+because of issue in block scopr and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
