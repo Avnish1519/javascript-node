b872252d5997a3af3b200685b3ba04d15d091da7
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-01-02 01:20:48 +0000

    Operations

---
+++ basics/Operations.js
@@ -0,0 +1,32 @@
+let value=3
+let negativeValue=-value
+console.log(negativeValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/2);
+console.log(2%2);
+
+let str1="Hello"
+let str2="Avnish"
+let str3=str1 + str2
+console.log(str3);
+
+console.log(1+"2");
+console.log("1"+2);
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);//Default value true after (+) -->1
+console.log(+"");//Default str after (+)--> 0
+
+let num1, num2 ,num3;
+num1=num2=num3=2+2;
+console.log(num1,num2,num3);
+
+let gameCounter =100
+gameCounter++          //Postfix increment
+console.log(gameCounter);
+
