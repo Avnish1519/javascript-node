84bd63acd1ba1fb4fc58ae71035dc93d3c94330c
CodeCosmos <165823302+Avnish1519@users.noreply.github.com>
2025-03-18 16:20:21 +0000

    comments

--- Arrays/02basics.js
+++ Arrays/02basics.js
@@ -5,18 +5,18 @@ const dc_heroes = ["Thor","IronMan","Hulk"]
 
 //console.log(marvel_heros);
 
-const allHeros = marvel_heros.concat(dc_heroes)
+const allHeros = marvel_heros.concat(dc_heroes)//constant declared
 console.log(allHeros);//object
 
 const all_new_heros= [...marvel_heros, ...dc_heroes]//way to concat two arrays
 console.log(all_new_heros);
 
-const another_array =[1,2,3,[4,5,6],7,[6,7]]
+const another_array =[1,2,3,[4,5,6],7,[6,7]]//array declared
 const real_another_array = another_array.flat(Infinity)//recusrion depth is given and it concat all subarrays
-console.log(real_another_array);
+console.log(real_another_array);//printing of array
 
 console.log(Array.isArray("Avnish"))//Tells if array or not
-console.log(Array.from("Avnish"));//Creates an array from object given
+console.log(Array.from("Avnish"));//Creates an array from object given    
 console.log(Array.from({name:"Avnish"}));//returns empty array
 
 let score1 = 100
